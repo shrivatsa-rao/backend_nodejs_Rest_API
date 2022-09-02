75aea183f601547d6a23effa25c523310e371b4f
Shrivtsa <68243174+shrivatsa-rao@users.noreply.github.com>
2022-09-02 17:39:08 +0530

    Delete data.js

--- data.js
+++
@@ -1,4 +0,0 @@
-const mongoose=require("mongoose");
-mongoose.connect('mongodb+srv://shree:shree@cluster0.nwxz4.mongodb.net/vishnu?retryWrites=true&w=majority',()=>{
-    console.log("connected to db");
-});
